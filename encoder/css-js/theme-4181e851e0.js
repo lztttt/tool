e2753548843abeb2858e40a7aba964bebb9e6b68
lztttt <73829571+lztttt@users.noreply.github.com>
2022-01-03 09:05:06 +0800

    Delete theme-4181e851e0.js

--- encoder/css-js/theme-4181e851e0.js
+++
@@ -1,1 +0,0 @@
-let theme=localStorage.getItem("Theme");theme||(theme=matchMedia("(prefers-color-scheme: dark)").matches?"Dark":"Light",localStorage.setItem("Theme",theme));"Dark"===theme?document.documentElement.classList.add("dark"):void 0;
